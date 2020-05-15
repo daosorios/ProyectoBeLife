4ed21061acce76441895f2fcaa5955178b027d5d
Javier Sabando <ja.sabando@alumnos.duoc.cl>
2020-05-15 00:36:48 -0400

    Styles

---
+++ ProyectoBeLife/Pruebas_Styles.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoBeLife
+{
+    /// <summary>
+    /// Lógica de interacción para Pruebas_Styles.xaml
+    /// </summary>
+    public partial class Pruebas_Styles : Page
+    {
+        public Pruebas_Styles()
+        {
+            InitializeComponent();
+        }
+    }
+}
